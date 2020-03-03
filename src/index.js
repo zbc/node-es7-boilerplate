dee2032b21bfe433324dc90b29e15fe1745c5eb1
Bocong Zhao <zhaobocong@hotmail.com>
2020-03-03 10:22:47 -0500

    Added comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+/**
+ * Returns the sum of a + b
+ *  
+ * @param {number} a first input number 
+ * @param {number} b second input number
+ * @return {number} the sum of a + b
+ */
+function sum(a, b) {
+  return a + b
+}
+
+console.log(sum(2, 3))
